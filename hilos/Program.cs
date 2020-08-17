1264256a3a15e033368319a16ef0b052187b27f5
Yoly06 <ymp.perez@yavirac.edu.ec>
2020-08-16 21:05:01 -0500

    Deber

---
+++ hilos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+ 
+namespace Programacion.Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+			
+        {
+            // Se instancia una variable con la clase de apoyo 'Objeto'
+            var EContratista = new Contratista();
+ 
+            // Se declaran los 2 hilos, estableciendo que la función a ejecutar será 'objetoE.ImprimeEnunciado'
+            Thread hilo1 = new Thread(EContratista.ImprimeEnunciado);
+            Thread hilo2 = new Thread(EContratista.ImprimeEnunciado);
+ 
+            // Se muestra el mensaje, desde el programa principal, que se llamarán a la ejecución de los hilos
+            // En la impresión de mensaje se especifica el identificador del hilo que se asigna internamente
+            Console.WriteLine("ID Hilo: {0} => Se llama a la ejecución de los hilos desde el programa principal", Thread.CurrentThread.ManagedThreadId.ToString());
+ 
+            // Se llaman a ejecución los 2 hilos, y se pasa como parámetro el mensaje un hecho para imprimir
+            hilo1.Start("Empresa: El 'Contratista tiene que firmar el documento");
+            hilo2.Start("Persona:Enseguida preparo los documentos");
+            
+			
+			
+            // Se espera a que termine la ejecución de ambos hilos
+            while ((hilo1.IsAlive) || (hilo2.IsAlive)) 
+            {
+            }
+ 
+            // Una vez terminados, se muestra el mensaje de finalización
+            Console.WriteLine("ID Hilo: {0} => Contrato finalizado", Thread.CurrentThread.ManagedThreadId.ToString());
+            Console.ReadLine();
+        }
+    }
+ 
+    // Clase de apoyo que contiene la función que ejecutarán los hilos
+    public class Contratista
+    {
+        public void ImprimeEnunciado(object texto)
+        {
+            // Se muestra el mensaje de ejecución de la función
+            // En la impresión de mensaje se especifica el identificador del hilo que se asigna internamente
+            Console.WriteLine("ID Hilo: {0} => {1}.", Thread.CurrentThread.ManagedThreadId.ToString(), texto.ToString());
+        }
+    }
+}
---
+++ paralelismo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace paralelismoY
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Declarar y crear un array
+            int[] Numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Normal foreach: ");
+            foreach (var numeros in Numeros)
+            {
+                //Impresion del 1 al 100
+                Console.WriteLine(longCalculation(numeros));
+            }
+           
+			//Salida en desorden
+            Console.WriteLine("Paralelismo foreach : ");
+            Parallel.ForEach(Numeros, numeros => {
+                Console.WriteLine(longCalculation(numeros));
+            });
+        }
+
+        private static int longCalculation(int numeros)
+        {
+            Thread.Sleep(100); 
+            return numeros * numeros;
+        }
+    }
+}
